7cbc9d51f4a27a79ff730d1b2db566fe3d6287e1
RISHABH KUMAR SONKAR <125142512+rishabh78601@users.noreply.github.com>
2023-07-20 09:53:49 +0530

    Add files via upload

---
+++ Python Project/Download-social-media-content-using-Libraries/insta_downloader.py
@@ -0,0 +1,11 @@
+# Download Video from youtube using pytube
+
+import instaloader
+
+profile_name = input("Enter Insta Profile name: ")
+
+print("Downloading Media...")
+
+instaloader.Instaloader().download_profile(profile_name, profile_pic_only= False)
+
+print("Download Completed")
